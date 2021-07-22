11d5e052e060360030fffad75d61872883753fd2
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-07-22 12:36:02 +0530

    Added tasks filtering

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,40 @@
 import { Template } from 'meteor/templating';
 import { TasksCollection } from '../api/TasksCollection';
+import { ReactiveDict } from 'meteor/reactive-dict'
+
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
 import './App.html';
 import './Task.js';
 
+Template.mainContainer.events({
+  "click #hide-completed-button"(Event, instance){
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  }
+});
+
+Template.mainContainer.onCreated(function mainContainer(){
+  this.state = new ReactiveDict();
+});
+
 Template.mainContainer.helpers({
   tasks() {
-    return TasksCollection.find({}, { sort: { createdAt: -1 } });
+    const instance = Template.instance();
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
+    return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: {createdAt: -1},
+    }).fetch();
+  },
+  hideCompleted() {
+    return Template.instance().state.get(HIDE_COMPLETED_STRING);
+  },
+  incompleteCount() {
+    const incompleteTasksCount = TasksCollection.find({ isChecked: {$ne: true}}).count();
+    return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
   },
 });
 
